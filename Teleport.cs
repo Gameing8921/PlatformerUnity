3bd70ccfc6222307db67a645400fa89fe8ea78af
Gameing8921 <159485488+Gameing8921@users.noreply.github.com>
2024-03-21 20:04:05 +0000

    Update Teleport.cs

--- Teleport.cs
+++ Teleport.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
+    // Get where to teleport to
     public Transform teleportPoint;
 
     void OnTriggerEnter(Collider collision)
     {
+        // Teleport to the set position
         collision.GetComponent<Transform>().position = teleportPoint.position;  
     }
 }
