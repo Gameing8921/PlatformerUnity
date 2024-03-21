2a7739bb8889fff4e26f8179c08b3929ab6ec758
Gameing8921 <159485488+Gameing8921@users.noreply.github.com>
2024-03-21 20:04:52 +0000

    Update Trampoline.cs

--- Trampoline.cs
+++ Trampoline.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 
 public class Trampoline : MonoBehaviour
 {
+    
     // Make a variable that controlls the data of the other script
     public Jump jump;
-    private void OnCollisionEnter(Collision collision)
+    // Make a variable that will store the jumpStrength in the future
+    public float jumpOld;
+    public void OnCollisionEnter(Collision collision)
     {
+        // Make the "jumpOld" variable store jumpStrength before the change
+        jumpOld = jump.jumpStrength;
         // Change the jumpStrength variable to 10 to increase jump
         jump.jumpStrength = 10;
-    }
+        
+}
 
     private void OnCollisionExit(Collision collision)
     {
         // Change it back to default
-        jump.jumpStrength = 2;
+        jump.jumpStrength = jumpOld;
     }
 }
