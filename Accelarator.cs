8f6d9f9922d06c8888ef8981e7dd03b74f947354
Gameing8921 <159485488+Gameing8921@users.noreply.github.com>
2024-03-21 20:01:22 +0000

    Add comments to Accelarator.cs

--- Accelarator.cs
+++ Accelarator.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class Accelarator : MonoBehaviour
 {
+    // Make a variable to store the movement script
     public FirstPersonMovement movement;
-
+    // Make a new variable storing the more speed there is
     public float speedFactor = 2.5f;
     private void OnTriggerEnter(Collider other)
     {
+        // Make the speed faster
         movement.runSpeed *= speedFactor;
     }
         
 
     private void OnTriggerExit(Collider other)
     {
+        // Change the speed back to normal
         movement.runSpeed /= speedFactor;
     }
 }
